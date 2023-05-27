ce5f13ac1349bbb500a54e76aa8858b0dd162727
Vladlen Popelenkov <popelenkow@gmail.com>
2023-05-27 12:50:21 +0700

    sfdfsd

---
+++ gh-pages/deploy.js
@@ -0,0 +1,9 @@
+const ghPages = require('gh-pages');
+
+ghPages.publish('dist', {
+	dotfiles: true,
+	history: false,
+	message: 'Deploy ed8vjvb3fr Application'
+}, (err) => {
+	console.error(err);
+});
